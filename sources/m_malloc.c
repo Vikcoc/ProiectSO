af48da5f4db7ae01cf5cc7c45903990d618ba410
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2020-12-25 02:47:12 +0200

    Initial implementation of malloc

--- sources/m_malloc.c
+++ sources/m_malloc.c
@@ -17,15 +17,34 @@ mem_block *mem_blocks_tail = NULL;
  * Implementation: Should first look if a free, big enough block already exists
  *                 with 'find_free_block'. If no such block is found,
  *                 'create_block' should be called.
- *                 The final size (not the one provided by the user) should
- *                 include the size of a mem_block and be aligned to ALIGNMENT.
+ *                 The size should be aligned to ALIGNMENT.
  *
- * Tip: Use 'size = (size + sizeof (mem_block) + ALIGNMENT - 1) & (~(ALIGNMENT -
- *      1))' to make size include the size of a block meta data and align it to
- *      ALIGNMENT.
+ * Tip: Use 'size = (size + ALIGNMENT - 1) & (~(ALIGNMENT - 1))' to align size
+ *      to ALIGNMENT.
  */
 
 void *m_malloc (size_t size)
 {
+    mem_block *ret_block;
 
+    if (size <= 0)
+        return (NULL);
+
+    size = (size + ALIGNMENT - 1) & (~(ALIGNMENT - 1));
+
+    ret_block = find_free_block (size);
+    if (ret_block != NULL)
+    {
+        ret_block = resize_block (ret_block, size);
+        ret_block -> free = false;
+    }
+    else
+    {
+        ret_block = create_block (size);
+
+        if (ret_block == NULL)
+            return (NULL);
+    }
+    
+    return (ret_block + 1);
 }
