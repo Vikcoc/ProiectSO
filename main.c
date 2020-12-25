5ad43b919432e78687c4f5bd203c06ac60f60640
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2020-12-25 02:46:49 +0200

    Added 'print_blocks' debug function

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include "includes/alloc.h"
 
+void debug_print_blocks ()
+{
+    mem_block *curr_block = mem_blocks_head;
+
+    printf ("\nHead: %p\nTail: %p\n", mem_blocks_head, mem_blocks_tail);
+    while (curr_block != NULL)
+    {
+        printf ("Block: status(%s)\t",  curr_block -> free ? "free" : "used");
+        printf ("size(%zu)\t", curr_block -> size);
+        printf ("location(%p)\t", curr_block);
+        printf ("prev(%p)\t", curr_block -> prev);
+        printf ("next(%p)\n", curr_block -> next);
+        curr_block = curr_block -> next;
+    }
+    printf ("\n");
+}
+
 int main ()
 {
     return (0);
