cb9d6eefa604df584fcc780b380d09a8a3501fb4
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2021-01-12 10:26:45 +0200

    Reverted main to an empty function

--- main.c
+++ main.c
@@ -1,30 +1,6 @@
 #include "includes/alloc.h"
 
-// void debug_print_blocks ()
-// {
-//     mem_block *curr_block = mem_blocks_head;
-
-//     printf ("\nHead: %p\nTail: %p\n", mem_blocks_head, mem_blocks_tail);
-//     while (curr_block != NULL)
-//     {
-//         printf ("Block: status(%s)\t",  curr_block -> free ? "free" : "used");
-//         printf ("size(%zu)\t", curr_block -> size);
-//         printf ("location(%p)\t", curr_block);
-//         printf ("prev(%p)\t", curr_block -> prev);
-//         printf ("next(%p)\n", curr_block -> next);
-//         curr_block = curr_block -> next;
-//     }
-//     printf ("\n");
-// }
-
 int main ()
 {
-    void *a = malloc (10 * sizeof (int));
-    void *b = calloc (10, sizeof (int));
-
-    a = realloc (a, 20 * sizeof(int));
-    free (a);
-    free (b);
-
     return (0);
 }
